09670a9c5a059b2cf933635a2802f6a8b80b6aab
Nia Nia <nil.nov4@gmail.com>
2020-02-12 14:43:26 +0700

    init

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	tb "gopkg.in/tucnak/telebot.v2"
+)
+
+func main() {
+	var (
+		port      = os.Getenv("PORT")       // set automatically
+		publicURL = os.Getenv("PUBLIC_URL") // from config vars
+		token     = os.Getenv("TOKEN")      // from config vars
+	)
+
+	webhook := &tb.Webhook{
+		Listen:   ":" + port,
+		Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
+	}
+
+	pref := tb.Settings{
+		Token:  token,
+		Poller: webhook,
+	}
+
+	b, err := tb.NewBot(pref)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b.Handle("/hello", func(m *tb.Message) {
+		b.Send(m.Sender, "Hi!")
+	})
+
+}
